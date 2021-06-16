c9c85a12f21a71938260c325b5bf904d3851b12f
Unay Santisteban <usantisteban@othercode.es>
2021-06-16 13:50:09 +0200

    Feature/Improved TypedCollection.

---
+++ src/Traits/HasTypeCheck.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComplexHeart\Domain\Model\Traits;
+
+/**
+ * Trait HasTypeValidation
+ *
+ * @author Unay Santisteban <usantisteban@othercode.es>
+ * @package ComplexHeart\Domain\Model\Traits
+ */
+trait HasTypeCheck
+{
+    /**
+     * Assert that the given value type match the required validType.
+     *
+     * @param  mixed  $value
+     * @param  string  $validType
+     *
+     * @return bool
+     */
+    protected function isValueTypeValid($value, string $validType): bool
+    {
+        if ($validType === 'mixed') {
+            return true;
+        }
+
+        $primitives = ['integer', 'boolean', 'float', 'string', 'array', 'object', 'callable'];
+        $validation = in_array($validType, $primitives)
+            ? fn($value): bool => gettype($value) === $validType
+            : fn($value): bool => $value instanceof $validType;
+
+        return $validation($value);
+    }
+
+    /**
+     * Assert that the given value type NOT match the required validType.
+     *
+     * @param  mixed  $value
+     * @param  string  $validType
+     *
+     * @return bool
+     */
+    protected function isValueTypeNotValid($value, string $validType): bool
+    {
+        return !$this->isValueTypeValid($value, $validType);
+    }
+}
